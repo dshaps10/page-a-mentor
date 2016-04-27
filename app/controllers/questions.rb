2eb4555197f3354a4213ca3f4ee5365a4257a074
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-04-26 19:42:56 -0700

    Clean up RESTful routes

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,10 +1,10 @@
-get '/questions/new' do
+get '/users/:id/questions/new' do
   @categories = Category.all
   @student = User.find(session[:user_id])
   erb :question
 end
 
-post '/questions' do
+post '/users/:id/questions' do
   user = User.find(session[:user_id])
   category = Category.find(params[:category])
 
@@ -12,5 +12,11 @@ post '/questions' do
                      summary: params[:summary],
                      details: params[:details],
                      location: params[:location])
-  redirect "/users/#{user.id}/profile"
+  redirect "/users/#{user.id}/questions"
+end
+
+get '/users/:id/questions' do
+  @user = User.find(params[:id])
+  @issues = Issue.where(user_id: @user.id)
+  erb :issues
 end
